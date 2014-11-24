f2a2ada9f278200f1344e5550930c7b687233af4
ebhemmanuel <ebh.emmanuel@gmail.com>
2014-11-24 01:32:00 -0500

    Adding my class work.

--- Day1/HTML5/js/Curves.js
+++ Day1/HTML5/js/Curves.js
@@ -1,13 +1,61 @@
-window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
+var myCyan           	 = "#88FFEB";
+var darkCyan           = "#76DECC";
+var mediumCyan         = "#7FEBD8";
+var myGreen            = "#25CC81";
+var darkGreen          = "#20B06F";
+var fillColor          = myGreen;
+var strokeColor        = darkGreen;
+var endPoint					 = "rgba(3,3,3,.25)";
+window.onload = function(){
+	var theCanvas    = document.getElementById('canvas1');
+	theCanvas.width  = 900;
+	theCanvas.height = 300;
+	if (theCanvas) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-						
-						
-						
-						
-						
+			ctx.strokeStyle = mediumCyan;
+			ctx.lineWidth   = 5;
+			ctx.lineCap = "round";
+			ctx.lineJoin = "round";
+
+			// Stroke a simple bezier curve
+			ctx.beginPath();
+			ctx.moveTo(50,200); // start point
+			ctx.bezierCurveTo(50,100,200,100,200,150)
+			ctx.stroke();
+
+			ctx.beginPath();
+			ctx.moveTo(150,200); // start point
+			ctx.bezierCurveTo(50,100,200,100,200,150)
+			ctx.stroke();
+
+			//show control points
+			ctx.strokeStyle = endPoint;
+			ctx.lineWidth   = 5;
+			ctx.beginPath();
+			ctx.moveTo(50,200);
+			ctx.lineTo(50,100);
+			ctx.lineTo(200,100);
+			ctx.lineTo(200,150);
+			ctx.stroke();
+
+			//stroke quadratic curve
+			ctx.strokeStyle = myGreen;
+			ctx.lineWidth   = 5;
+			ctx.beginPath();
+			ctx.moveTo(400,200);
+			ctx.quadraticCurveTo(400,100,600,150);
+			ctx.stroke();
+
+			//show control points
+			ctx.strokeStyle = endPoint;
+			ctx.lineWidth   = 5;
+			ctx.beginPath();
+			ctx.moveTo(400,200);
+			ctx.lineTo(400,100);
+			ctx.lineTo(600,150);
+			ctx.stroke();
+
 		}
 	}
 }
--- Day1/HTML5/js/Text.js
+++ Day1/HTML5/js/Text.js
@@ -1,13 +1,34 @@
-window.onload = function() {
-	var theCanvas = document.getElementById('Canvas1');
-	if (theCanvas && theCanvas.getContext) {
-	var ctx = theCanvas.getContext("2d");
+var myCyan         	 = "#88FFEB";
+var darkCyan         = "#76DECC";
+var mediumCyan       = "#7FEBD8";
+var myGreen          = "#25CC81";
+var darkGreen        = "#20B06F";
+var fillColor        = myGreen;
+var strokeColor      = darkGreen;
+var endPoint				 = "rgba(3,3,3,.25)";
+window.onload        = function(){
+	var theCanvas      = document.getElementById('canvas1');
+		theCanvas.width  = 900;
+		theCanvas.height = 300;
+	if (theCanvas) {
+	var ctx            = theCanvas.getContext("2d");
 	if (ctx) {
-						
-						
-						
-						
-						
+		var theString    = "Drawing Text on a Canvas";
+		// Draw a simple text string on canvas using default settings
+		ctx.fillText(theString,20,20);
+		// Draw the string with some font info
+		ctx.font         = "25pt Helvetica";
+		ctx.fillText(theString,20,60);
+		// Draw the string with a stillStyle color
+		ctx.fillStyle    = myGreen;
+		ctx.fillText(theString,20,100);
+		// Draw the string with a stroke and a fill with some opacity
+		ctx.font         = "32pt Verdana";
+		ctx.fillStyle    = darkCyan;
+		ctx.strokeStyle  = "rgba(127, 235, 216, 0.25)";
+		ctx.lineWidth    = 6;
+		ctx.fillText(theString,20,160);
+		ctx.strokeText(theString,20,160);
 		}
 	}
 }
